504d44df7c47d51d4a30c9ebc3a9fbefd81e45e6
Jonas Vetsch <jonas@vetsch.com>
2024-04-16 15:36:43 +0630

    Initial Commit

---
+++ MySwitzerland/MySwitzerlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwitzerlandApp.swift
+//  MySwitzerland
+//
+//  Created by Jonas Vetsch on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySwitzerlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
